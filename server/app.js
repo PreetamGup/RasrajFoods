1e77c52cd9bea1b7f031ca8202e1e0273c81bca6
Preetam Gupta <106731801+PreetamGup@users.noreply.github.com>
2025-01-02 19:36:52 +0530

    added redis url for vercel

--- server/app.js
+++ server/app.js
@@ -27,8 +27,8 @@ export let redisClient;
 
 (async () => {
   redisClient = createClient({
-  
-  });
+   url:process.env.REDIS_URL,
+});
 
   redisClient.on("error", (error) => console.error(`Error : ${error}`));
 
