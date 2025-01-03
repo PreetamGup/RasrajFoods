1245543522f9d917a1b407d099a22d23b231b532
Preetam Gupta <106731801+PreetamGup@users.noreply.github.com>
2025-01-03 22:19:35 +0530

    latest

--- server/app.js
+++ server/app.js
@@ -15,9 +15,9 @@ dotenv.config();
 export const app = express();
 
 app.use(logger('dev'));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use(cors(
     {
